59044d4f5828823c44244d04486a133b656ff3c8
Adnan Hidayat <komputok@gmail.com>
2020-03-03 15:50:43 +0700

    - Revert filesaver to original

--- dist/FileSaver.js
+++ dist/FileSaver.js
@@ -65,7 +65,7 @@
   function corsEnabled(url) {
     var xhr = new XMLHttpRequest(); // use sync to avoid popup blocker
 
-    xhr.open("GET", url, false);
+    xhr.open("HEAD", url, false);
 
     try {
       xhr.send();
--- src/FileSaver.js
+++ src/FileSaver.js
@@ -55,7 +55,7 @@ function download(url, name, opts) {
 function corsEnabled(url) {
   var xhr = new XMLHttpRequest();
   // use sync to avoid popup blocker
-  xhr.open("GET", url, false);
+  xhr.open("HEAD", url, false);
   try {
     xhr.send();
   } catch (e) {}
