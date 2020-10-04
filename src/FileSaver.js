ed0326b0a8590c1792323172e8b7cdda493da3be
Adnan Hidayat P <komputok@gmail.com>
2020-10-04 14:37:33 +0700

    - remove console.log

--- src/FileSaver.js
+++ src/FileSaver.js
@@ -84,10 +84,6 @@
     xhr.onload = function () {
       var isCorsEnable = xhr.status >= 200 && xhr.status <= 299;
       isCorsEnable ? onEnable() : onDisable();
-      console.log(xhr.status);
-      console.log(onEnable);
-      console.log(onDisable);
-      console.log(xhr.response);
     };
 
     try {
